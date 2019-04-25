2f23dcad79d4f8455191c27c6024459070174f50
Jiang <827081758@qq.com>
2019-04-25 20:26:55 +0800

    c=a+b

---
+++ c=a+b.asm
@@ -0,0 +1,41 @@
+功能：将a和b段中存储的数据依次相加，存到c段中去
+assume	cs:code
+
+a segment
+	db 1,2,3,4,5,6,7,8
+a ends
+
+b segment
+	db 1,2,3,4,5,6,7,8
+b ends
+
+c segment
+	db 0,0,0,0,0,0,0,0
+c ends
+
+code segment
+	start:
+		mov ax,a
+		mov es,ax
+		mov	ax,c
+		mov ds,ax
+		mov bx,0
+		mov cx,8
+	s1:
+		mov ax,es:[bx]
+		add	[bx],ax
+		add	bx,2
+		loop s1
+		mov ax,b
+		mov	es,ax
+		mov	bx,0
+		mov	cx,8
+	s2:
+		mov	ax,es:[bx]
+		add	[bx],ax
+		add	bx,2
+		loop s2
+		mov ax,	4c00h
+		int 21h
+code ends
+end start
